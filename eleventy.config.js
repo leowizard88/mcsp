1e2deae853a204e586b4ddec3800e8eec0105c06
leowizard88 <leonardo.savio2002@gmail.com>
2026-07-06 19:39:24 +0200

    add json template filter

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
   eleventyConfig.addPassthroughCopy({ "src/admin": "admin" });
   eleventyConfig.addFilter("limit", (arr, n) => (arr || []).slice(0, n));
+  eleventyConfig.addFilter("json", value => JSON.stringify(value || "").replace(/</g, "\\u003c"));
   eleventyConfig.addFilter("dateISO", d => {
     if (!d) return "";
     const date = new Date(d);
