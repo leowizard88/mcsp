722ea4c569cfafd3709c897f6b730469fbe92c56
leowizard88 <leonardo.savio2002@gmail.com>
2026-07-07 01:59:13 +0200

    bust stable ornament script cache

--- src/assets/js/article-ornament-column.js
+++ src/assets/js/article-ornament-column.js
@@ -1,6 +1,6 @@
 (() => {
   const script = document.createElement('script');
-  script.src = '/assets/js/article-ornament-column-stable.js?v=20260706-1';
+  script.src = '/assets/js/article-ornament-column-stable.js?v=20260706-2';
   script.defer = true;
   document.head.appendChild(script);
 })();
